8f86684513ab5f7868757fee67603b1f353ad0c2
arikkp01 <30876682+arikkp01@users.noreply.github.com>
2020-02-07 17:23:12 -1000

    Update sqlUpdate.py

--- sqlUpdate.py
+++ sqlUpdate.py
@@ -21,3 +21,7 @@ db.commit()
 #disconnect
 cur.close()
 db.close()
+
+# Most of my files will have a table called age2. 
+# In my phpmyadmin, in my database students,
+#I acidentally labelled a table age2 and never changed it
