706ef616f407cf57de257d7d38b35f386b277887
arikkp01 <30876682+arikkp01@users.noreply.github.com>
2020-02-07 16:53:08 -1000

    Create sqlInsert.py

---
+++ sqlInsert.py
@@ -0,0 +1,27 @@
+import pymysql
+
+#connecting database
+db = pymysql.connect("localhost", "root", "password", "classroom")
+
+#preparing object with cursor() method
+cur = db.cursor(pymysql.cursors.DictCursor)
+
+# entering new data
+sql1 = "INSERT INTO students (name,age2,gradeLevel) VALUES ('Joe', 18, 9)"
+
+# executing SQL query with execute() method
+cur.execute(sql)
+db.commit()
+#printing record
+print(mycursor.rowcount, "record inserted.")
+
+sql2 = "SELECT * from students"
+cur.execute(sql2)
+
+data = cur.fetchall()
+for row in data:
+	print("Name: " + row['name'] + " Age: " + str(row['age2']) + " Grade level: " + str(row['gradeLevel'])) 
+
+#disconnecting server
+cur.close()
+db.close()
